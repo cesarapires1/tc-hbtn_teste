d67c9f9eea0c9bc83ec4c1c57262e7cd6bc7dc12
Cesar A. Pires <cesarp@ciandt.com>
2022-06-21 10:51:02 -0300

    Projeto dia 31 Task 0. Email Junit

--- 0x01/email/src/Pessoa.java
+++ 0x01/email/src/Pessoa.java
@@ -1,5 +1,3 @@
-package org.example;
-
 public class Pessoa {
     public static boolean emailValid(String email){
         if (email.length() <= 50 || email.contains("@")) {
--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,23 +1,21 @@
-package org.example;
-
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class TesteEmail extends TestCase {
+public class TesteEmail {
     @Test
-    public static void testar_email_com_arroba() {
+    public void testar_email_com_arroba() {
         boolean isValid =  Pessoa.emailValid("email_teste@dominio.com.br");
         assertTrue(isValid);
     }
 
     @Test
-    public static void testar_email_sem_arroba() {
+    public void testar_email_sem_arroba() {
         boolean isValid = Pessoa.emailValid("email_testedominio.com.br");
         assertTrue(isValid);
     }
 
     @Test
-    public static void testar_email_mais_50_caracteres() {
+    public void testar_email_mais_50_caracteres() {
         boolean isValid = Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
         assertEquals(true, isValid);
     }
